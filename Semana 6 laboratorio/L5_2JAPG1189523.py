4265b1212abf427becbbd409d92e33864798a372
Puaque <143530922+Puaque@users.noreply.github.com>
2023-09-12 09:56:10 -0600

    Add files via upload

---
+++ Semana 6 laboratorio/L5_2JAPG1189523.py
@@ -0,0 +1,21 @@
+print("Ejercicio 2")
+
+X=input("Ingrese un número de 1 a 7: ")
+xnumero=int(X)
+
+if xnumero==1:
+    print("El día es Lunes")
+elif xnumero==2:
+    print("El día es Martes")
+elif xnumero==3:
+    print("El día es Miercoles")
+elif xnumero==4:
+    print("El día es Jueves")
+elif xnumero==5:
+    print("El día es Viernes")
+elif xnumero==6:
+    print("El día es Sábado")
+elif xnumero==7:
+    print("El día es Domingo")
+else:
+    print("ERROR, el número ingresado debe ser de uno a siete")
