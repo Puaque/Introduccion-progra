23edcd4a8d738b0d6284dd7aecf2a73e0c69f7a7
Puaque <143530922+Puaque@users.noreply.github.com>
2023-09-12 09:52:15 -0600

    Add files via upload

---
+++ L5_1JAPG1189523.py
@@ -0,0 +1,10 @@
+print("Ejercicio 1")
+X = input("Ingrese un número entero: ")
+xnumero=int(X)
+
+if xnumero==0:
+    print ("Resultado: CERO")
+elif xnumero < 0:
+    print("Resultado: NEGATIVO")
+else:
+    print("Resultado: POSITIVO")
